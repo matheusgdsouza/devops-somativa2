aaee98aafcaf753ec5dedd9e57da200be38403c9
matheusgdsouza <matheusgds3011@gmail.com>
2025-04-27 23:33:50 -0300

    adicionar-saudacao

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
-
+import random
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": True, "Hello World": "num_aleatorio", "num_aleatorio": random.randint(1, 100)}
+
+@app.get("/saudar/{nome}")
+async def saudar_nome(nome: str):
+    return {"mensagem": f"Olá, {nome}! Bem-vindo(a) à nossa API."}
